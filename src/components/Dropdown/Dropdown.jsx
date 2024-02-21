97aa7f257578a49eae4632a162cdbe8a17e3e2ec
Lance Turbes <instinct_truster@protonmail.com>
2024-02-21 11:16:31 -0800

    add svgs

---
+++ src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,11 @@
+import styles from "./Dropdown.module.css";
+
+function Dropdown() {
+  return (
+    <div className={styles.dropdown}>
+      <div className={styles.dropdownHeader}>header</div>
+    </div>
+  );
+}
+
+export default Dropdown;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,17 @@
 import Button from "../Button/Button";
 import Card from "../Card/Card";
+import Dropdown from "../Dropdown/Dropdown";
 import Typography from "../Typography/Typography";
 import styles from "./Form.module.css";
 
 function Form() {
   return (
     <div className={styles.form}>
-      <Typography fontSize={24} bold>
-        Add new Goal
-      </Typography>
+      <div className={styles.titleContainer}>
+        <Typography fontSize={24} bold>
+          Add new Goal
+        </Typography>
+      </div>
       <Card col>
         <div className={styles.inputContainer}>
           <Typography fontSize={16}>Goal Name:</Typography>
@@ -16,7 +19,7 @@ function Form() {
         </div>
         <div className={styles.inputContainer}>
           <Typography fontSize={16}>Category:</Typography>
-          <input type="text" />
+          <Dropdown />
         </div>
         <Button />
       </Card>
